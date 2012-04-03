6dcc293a6e4a9ecaf401b62c315bdee6c58d069e
Jun Sakai <sakaijunsoccer@gmail.com>
2012-04-03 19:17:58 +0000

    Git Test

--- testcookbook/metadata.rb
+++ testcookbook/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "sakaijunsoccer@gmail.com"
 license          "BSD"
 description      "Installs/Configures testcookbook"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "1.0.1"
 
 recipe "testcookbook", "Test Cook Book"
